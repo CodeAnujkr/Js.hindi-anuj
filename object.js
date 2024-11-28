620b8e356664f6f540b3b41c88407787249fb600
CodeAnujkr <anujprajpati2011@gmail.com>
2024-11-28 17:44:46 +0530

    Create object.js
    first object

---
+++ object.js
@@ -0,0 +1,69 @@
+// singtom
+// const tinderuser = new Object()
+const tinderuser = {}
+tinderuser.name = "123abs"
+tinderuser.islogged = false
+// console.log(tinderuser);
+
+
+
+const regular ={
+    email: "anuj@gamil.com",
+    fullname:{
+        fisrtname:"anuj",
+        secondname:"amit"
+
+    }
+}
+// console.log(regular.fullname.fullname.secondname);
+const obj1 = {1:"a " ,2:"b"}
+const obj2 = {3:"a " ,4:"b"}
+
+// const obj3 = {obj1 , obj2}
+// const obj3 = Object.assign({},obj1,obj2)
+// console.log(obj3);
+
+const obj3 = {...obj1,...obj2}
+const user = [
+    {
+        id : 1,
+        email : "anujprajapati2011@gamilcom"
+    },
+ 
+    {
+        id : 1,
+        email :"anujprajapati2011@gamil.com"
+    }
+]
+user [1].email
+//console.log(tinderuser);
+//console.log(Object.keys(tinderuser));
+//console.log(Object.values(tinderuser));
+///console.log(Object.entries(tinderuser));
+//console.log(tinderuser.hasownproperty('islohgged'));
+
+
+
+
+const couse = {
+cousename : "js name",
+price :"999",
+couseteacher :"anuj"
+}
+
+// couse.couseteacher
+const { couseteacher: teacher} = couse
+// console.log(teacher);
+// {
+//    " name" :"anuj"/
+    // couse" :"js"
+    // price : "free"
+// }
+
+[
+
+    {},
+    {},
+    {}
+]
+GainNode.caller(cancelAnimationFrame)
