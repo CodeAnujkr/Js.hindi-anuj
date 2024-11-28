45933ccecaa927ee1c9b99d11b4ef7b3c8afaef1
CodeAnujkr <anujprajpati2011@gmail.com>
2024-11-28 17:46:10 +0530

    Create number-maths.js
    number and maths

---
+++ number-maths.js
@@ -0,0 +1,41 @@
+ const  score = 400
+// console.log(score)
+ const balance = new Number(100)
+ //console.log(balance)
+
+
+// console.log(balance.toString().length);
+ //console.log(balance.toFixed(1)); 
+ const otherNumber = 123.8966 
+ //console.log(otherNumber.toPrecision(4));
+
+ const hunderd = 1000000
+ //console.log(hunderd.toLocaleString('en-IN'))
+ 
+// ++++++++++++ math+++++++++++++++
+
+//console.log(Math);
+//console.log(Math.abs(-4))
+
+//console.log(Math.round(4.6));
+//console.log(Math.ceil(4.6));
+//console.log(Math.floor(4.6));
+//console.log(Math.sqrt(4.6));
+
+//console.log(Math.min(4.6));
+//console.log(Math.max(4.6));
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()* (max - max + 1)) + min)
+
+
+
+
+
+
+
