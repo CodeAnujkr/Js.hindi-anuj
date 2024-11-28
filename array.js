3e77327867492b7babb77a6a0502c76da31c8813
CodeAnujkr <anujprajpati2011@gmail.com>
2024-11-28 17:38:50 +0530

    Create array.js
    Array

---
+++ array.js
@@ -0,0 +1,34 @@
+ const merver_hero = ["hero","amit","ahash","sncide"]
+
+ const dc= ["average","ansh","aditya","sncideds"]
+
+//  merver_hero.push(dc)
+
+//  console.log(merver_hero);
+//  console.log(merver_hero[3][1]);
+
+// const allmerver_hero.concat(dc)
+
+//  console.log(allmerver_hero);
+
+const all  =  [...merver_hero ,... dc]
+console.log(all);
+
+const another = [1,2,3,4,[1,1,2,3,],7 [1,3,4,5]]
+const real = another.flat(Infinity)
+console.log(real);
+
+
+ 
+console.log(Array.isArray('anuj'))
+console.log(Array.from('anuj'))
+console.log(Array.from({name : "anuj"}))
+
+let scroe1 = 100
+let scroe2 = 200
+let scroe3 = 300
+
+// console.log(Array.of(scroe1,scroe2,scroe3)
+
+
+
