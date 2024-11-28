8d28f9046c666d72cd6ae5e0f1fa6d2d87d811c6
CodeAnujkr <anujprajpati2011@gmail.com>
2024-11-28 17:43:16 +0530

    Create arry-method.js
    method

---
+++ arry-method.js
@@ -0,0 +1,32 @@
+// array
+const myarr = [0,1,2,3,4,5, true ,"anuj"]
+const myhearo = ["anuj","anuj"]
+//const myarr2 = new Array[1,2,3,4,]
+// console.log();
+
+// array method
+
+// myarr.push(6)
+// myarr.pop(6)
+// myarr.push76)
+// myarr.unshift(9)
+// myarr.shift()
+//console.log(myarr.includes(9));
+//console.log(myarr.indexOf(9));
+//console.log(myarr.includes(9));
+
+//const newarry = myarr.join()
+//console.log(myarr)
+//console.log(newarry)
+
+console.log("a",myarr);
+const emyn1 =  myarr.slice(1,3)
+console.log("b",myarr);
+
+const emyn2 =  myarr.splice(1,3) 
+console.log(emyn2);
+
+
+
+
+
