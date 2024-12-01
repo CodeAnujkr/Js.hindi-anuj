a362af9cd926d6846ef404d19801195f4664c687
CodeAnujkr <anujprajpati2011@gmail.com>
2024-12-01 16:53:52 +0530

    Create scope.js
    scope

---
+++ scope.js
@@ -0,0 +1,58 @@
+// if (true) {
+// let a = 10
+// const b = 30
+//   var c = 20
+// }
+let a = 20
+const b = 30
+var c = 40
+// console.log(a);
+// console.log(b);
+
+// for (let index = 0; index < array.length; index++) {
+// const element = array[index];
+
+// }
+// console.log(c);
+
+//var c =300
+// let a = 300
+function one() {
+    const username = "anuj"
+    function two() {
+        const wedsite = "youtude"
+        console.log(username)
+    }
+    // console.log(wedsite)
+
+    two()
+}
+// one()
+
+if (true) {
+    const username = "anuj"
+    if (username === "anuj") {
+        const wedsite = " youtude"
+        // console.log(username + wedsite);
+
+
+
+    }
+    // console.log(wedsite);
+
+}
+// console.log(username);
+
+// +++++++++++++++++++++++++++++++++++ intersecting ++++++++++++++
+console.log(addone(5));
+
+function addone(num) {
+    return num+ 1;
+}
+// addone(5)
+
+const addtwo = function(num){
+    return num +1;
+
+}
+addtwo(5)
