9015aedd3d4404b4bb989de4123ef1ce62d32311
CodeAnujkr <anujprajpati2011@gmail.com>
2024-12-05 18:34:40 +0530

    Create for-loop.js
    for-loop

---
+++ for-loop.js
@@ -0,0 +1,83 @@
+//for
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    // console.log(element);
+ if (element == 5) {
+    // console.log("5 is best number");
+    
+ }   
+    
+}
+
+for (let i = 1; i <=10; i++) {
+    // const element = array[i];
+    // console.log(`outer loop ${i}`);
+    
+    for (let j = 1; j <+ 10; j++) {
+        // const element = array[j];
+        // console.log(`outer loop ${j} and inner loop ${i}`);
+        // console.log(i + '*'+ j + ' = ' + i*j);
+        
+   
+    }
+}
+// let myarray = ["KKR","CSK","LSG", "RRR"]
+// console.log(myarray.length);
+
+// for (let index = 0; index < myarray.length; index++) {
+    // const element = myarray[index];
+    // console.log(element);
+    
+    
+// }
+// key word
+ // brek ant continue
+
+//  for (let index = 1; index <= 20; index++) {
+//     // const element = array[index];
+//     if (index == 5) {
+//         console.log("Detcted 5");
+//         break ;
+        
+//     }
+//     console.log(`value of i is ${index}`);
+
+    
+    
+//  }
+
+
+ for (let index = 1; index <= 20; index++) {
+    // const element = array[index];
+    if (index == 5) {
+        console.log("Detcted 5");
+        continue ;
+        
+    }
+    console.log(`value of i is ${index}`);
+
+    
+    
+ }
+  // while loop
+let index =0
+ while (index <= 10) {
+    console.log(`value of index ${index}`);
+     index =index+2;
+    
+ }
+
+//  let myarray = ["KKR","CSK","LSG", "RRR"]
+// let arr =0
+//  while (arr<myarray.length) {
+//     // console.log(`value is ${myarray[arr]}`);
+//     arr = arr + 1;
+    
+    
+//  }
+let score = 11
+do {
+console.log(`score is ${score}`);
+    score++;
+} while (score <= 10)
